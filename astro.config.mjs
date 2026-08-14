a9309f79aeb0583b51d6f8cf04f1ae6cbf8a3189
jam2342 <95386665+jam2342@users.noreply.github.com>
2026-08-13 23:32:13 -0500

    Fix production canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://portfolio-wine-three-iox2jn0bx6.vercel.app',
+});
